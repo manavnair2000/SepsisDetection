2ba596d4fb2aea4ab61e38fd2d4cd77ca907c09b
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-01-04 18:49:39 +0530

    frontend

---
+++ Input/Website/js/vitals.js
@@ -0,0 +1,10 @@
+function calculate(){
+var num1 = document.getElementById("temperature").value;
+var num2 = document.getElementById("heartrate").value; 
+var num3 = document.getElementById("wbc").value;
+var num4 = document.getElementById("respiratory rate").value;  
+var num1 = document.getElementById("systolicbp").value;
+var num2 = document.getElementById("respiratoryrate").value;  
+var num1 = document.getElementById("alteredmentation").value;
+   
+}
